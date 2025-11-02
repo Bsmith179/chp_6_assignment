5a27adedb017d74f136da4e0c21534392eb864ac
Bsmith179 <bsmith179@student.cscc.edu>
2025-11-02 07:17:25 -0500

    Added raw folder to resources for audio files.

--- app/src/main/java/com/example/lab6_bsmith179_1/MainActivity.java
+++ app/src/main/java/com/example/lab6_bsmith179_1/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.lab6_bsmith179_1;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button1, button2;
+    MediaPlayer mpSad, mpCats;
+    int playing;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,27 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        button1 = (Button) findViewById(R.id.btSad);
+        button2 = (Button) findViewById(R.id.btCats);
+        button1.setOnClickListener(Sad);
+        button2.setOnClickListener(Cats);
     }
+    Button.OnClickListener Sad = new Button.OnClickListener(){
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    };
+
+    Button.OnClickListener Cats = new Button.OnClickListener(){
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    };
+
+
 }
