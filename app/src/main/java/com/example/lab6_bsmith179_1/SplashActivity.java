f7f31d27d8259d088583db1fc02241569ae7bed9
Bsmith179 <bsmith179@student.cscc.edu>
2025-11-02 05:04:58 -0500

    Created timer for 5 second splash screen transition to main activity.

--- app/src/main/java/com/example/lab6_bsmith179_1/SplashActivity.java
+++ app/src/main/java/com/example/lab6_bsmith179_1/SplashActivity.java
@@ -1,5 +1,6 @@
 package com.example.lab6_bsmith179_1;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,6 +9,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
@@ -15,6 +19,17 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
+        TimerTask task = new TimerTask(){
+
+            @Override
+            public void run() {
+                finish();
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            }
+        };
+        Timer opening = new Timer();
+        opening.schedule(task,5000);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
